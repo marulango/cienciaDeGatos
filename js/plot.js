be6dc185d3d3ceb4820594d9f4886971a0b3b824
marulango <hola@maruma.ru>
2017-03-23 16:12:42 -0600

    Primera gráfica: casi lista

--- js/plot.js
+++ js/plot.js
@@ -1,11 +1,37 @@
 var barWidth = 30
+var sunsetOrange = '#FF883E'
+var clearedGreen = '#79DD15'
+var brand = ['#EF2B4C','#FF3E5F', '#FF7190', '#FF9CB6', '#FFC7D8']
+var guidanceBlue = '#3F91FF'
+var main = '#939598'
+
 
 function totalPopBars() {
     for (var i = 0; i < nombreEstado.length; i++) {
+        stroke(brand[1])
         var xPosition = (barWidth * i + 10)
-        fill(0)
-        rect(xPosition, 700, 20, -poblacionTotal[i])
+        fill(brand[3])
+        rect(xPosition, 600, 20, -poblacionTotal[i])
     }    
+}
+
+function totalPetsBars() {
+for (var i = 0; i < statePets.length; i++) {
+        stroke(brand[1])
+        var xPosition = (barWidth * i + 10)
+        fill(brand[1])
+        rect(xPosition, 600, 20, -statePets[i])
+    }
+}
+
+function stateLabels() {
+    fill(130)
+    rotate(1.5707963268) //radianes porque YOLO
+    translate(0, -15)
+    noStroke()
+    for (var i = 0; i < nombreEstado.length; i++) {
+        text(nombreEstado[i], 620, (-30 * [i]))
+    }
 }
 
 function setup() {
@@ -14,6 +40,8 @@ function setup() {
 
 function draw() {
     totalPopBars()
+    totalPetsBars()
+    stateLabels()
     noLoop()
 }
 
